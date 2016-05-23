23b0185b10fc01fc3822ac670e9f4b2e6e097b7e
Dante <przemek.rzedkowski@interia.eu>
2016-05-24 00:45:46 +0200

    Zaczynamy po mału zapełniać wczytywanie.

---
+++ src/Area/Area.java
@@ -0,0 +1,16 @@
+package Area;
+
+/**
+ * Created by Dante on 23.05.2016.
+ */
+public abstract class Area {
+
+    protected int type;
+    protected int h;
+    protected Point[] points;
+
+    public abstract Point[] create(int... dimension);
+    public void dupa() {
+
+    }
+}
---
+++ src/Area/Point.java
@@ -0,0 +1,14 @@
+package Area;
+
+/**
+ * Created by Dante on 23.05.2016.
+ */
+public class Point {
+
+    private int value;
+
+    public Point(int value) {
+
+        this.value = value;
+    }
+}
