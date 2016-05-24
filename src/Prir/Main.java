dbdf33148279d841fe328d9b22c4b60ecf7340f2
jakubgac <gacus65@gmail.com>
2016-05-24 18:27:21 +0200

    Main do uruchamiania

--- src/Prir/Main.java
+++ src/Prir/Main.java
@@ -2,15 +2,20 @@ package Prir;
 
 import Loading.DataLoading;
 import Loading.Function;
+import Area.Area;
 
 public class Main {
 
 	public static void main(String []args) throws Exception {
 		// żeby tylko potestować jak działa wczytywanie funkcji 
-		/* Function function;
+		Function function;
+		Area area;
+		
 		DataLoading data = new DataLoading();
 		
 		function = data.FunctionLoading();
-		System.out.println(function.get(1.0, 2.0)); */
+		System.out.println(function.get(1.0, 2.0));
+		area = data.AreaLoading();
+		
 	}
 }
