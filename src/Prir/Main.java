eb568e49285e19a80bf4a583c7d68a4867783567
jakubgac <gacus65@gmail.com>
2016-05-29 13:03:56 +0200

    Wczytywanie danych - kompletny Main

--- src/Prir/Main.java
+++ src/Prir/Main.java
@@ -1,23 +1,20 @@
 package Prir;
 
-import java.util.List;
-
-import Condition.Condition;
+import BorderPoints.BorderPoints;
 import Loading.DataLoading;
 
 public class Main {
 
 	public static void main(String []args) throws Exception {
 		
-		// code for testing data loading
+		// code for data loading
 		DataLoading data = new DataLoading();
 		// reading functions
-		//data.FunctionLoading();
+		data.FunctionLoading();
 		// reading area
 		data.AreaLoading();
 		// reading conditions
 		data.ConditionLoading();
-		List<Condition> conditions = data.getConditionsList();
-		System.out.println(conditions.toString());
+		// since this moment, reading is all done
 	}
 }
