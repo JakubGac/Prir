dc3eb29f04ab802b73efbc4bba4dd48ec82b2a0d
jakubgac <gacus65@gmail.com>
2016-05-29 12:58:55 +0200

    Dodanie klasy Matrix i CreateMatrix

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -1,51 +1,39 @@
 package Matrix;
-import Area.*;
-import Loading.*;
-/**
- * Created by Dante on 26.05.2016.
- */
+
 public class Matrix {
+    private int numberOfColumns;
+    private int numberOfRows;
+    private double values[][];
 
-    private Point[] x;
-    private Double[][] A;
-    private Double[] B;
-    private static int size;
-
-    public Matrix() {
-        this.x = new Point[size];
-        this.A = new Double[size][size];
-        this.B = new Double[size];
+    public Matrix(int rows, int columns) {
+    	this.numberOfRows = rows;
+    	this.numberOfColumns = columns;
+        values = new double[rows][columns];
     }
 
-    public void fill_matrix(Area area, Function function) {
-
-        int sizeA = (int)area.get_sizes_array().get(0);
-        int sizeB = (int)area.get_sizes_array().get(1);
-        int k = 0;
-
-        for(int i = 0; i < sizeA; i++) {
-            for(int j = 0; j < sizeB; j++) {
-                if(area.getPoint(i, j).getIf_edge() == true) { //wypelniam macierz x "niewiadomymi"
-                    x[k++] = area.getPoint(i, j);
-                }
-            }
-        }
-
-        if (k == 0) {System.out.println("podaj lepsze wymiary gupi uzytkowniku\n"); return;}
-
-        for(int i = 0; i < k; i++) {
-            B[i] = function.get((double)x[i].getX(), (double)x[i].getY()) - 4/Math.pow(area.getH(), 2); //wypelniam macierz prawych stron danymi
-        }
-
-        for(int i = 0; i < k; i++) {
-            for(int j = 0; j < k; j++) {
-
-            }
-        }
+    public void setValue(int row, int column, float value) {
+        values[row][column] = value;
+    }
+    
+    public double getValue(int row, int column) {
+        return values[row][column];
     }
 
+    public synchronized int getRows() {
+        return numberOfRows;
+    }
 
-    public static void setSize(int my_size) {
-        size = my_size;
+    public synchronized int getCols() {
+        return numberOfColumns;
+    }
+    
+    public void printMatrix() {
+    	for(int i=0 ; i < numberOfRows ; i++){
+    		for(int j=0 ; j < numberOfColumns ; j++){
+    			System.out.print(values[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
+    	System.out.println();
     }
 }
